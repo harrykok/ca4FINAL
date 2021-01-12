765067468884b87d9022222a0bfd34f781f5c104
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2021-01-13 02:58:07 +0800

    initial state v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,37 @@
-function addTracking(){
-    document.getElementById("grid-container").innerHTML += '<div class="grid-item"><button class="plus-button" onclick=addTracking()></button><a>Add another</a></div>'
+trackerId = 1;
+
+function addTracking() {
+    document.getElementById("addTracking").remove();
+    document.getElementById("grid-container").innerHTML +=
+        '<div class="grid-item tracking" id="tracker' + trackerId+ '">' +
+        '<table class="tableAlign">' +
+        '<form action="#" onsubmit="getQueueApi();return false">' +
+        '<tr>' +
+        '<td><input type="text" id="companyId" placeholder="Company Id"></td>' +
+        '<td><input type="submit" id="search" value="Search"></td>'+
+        '<td><span class="cross" onclick="closeTracking(' + trackerId + ')">x</span></td>' +
+        '<td id="closeTrackingCross"></td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td><select id="queueId">' +
+        '</select></td>' +
+        '<td colspan="2"><input type="checkbox" id="hideInactive" checked>' +
+        '<label for="hideInactive">Hide Inactive</label></td>' +
+        '</tr>' +
+        '</form>' +
+        '</table>' +
+        '</div>';
+    document.getElementById("grid-container").innerHTML +=
+        '<div class="grid-item" id="addTracking" onclick=addTracking()>' +
+        '<span class="circle plus"></span><a class="addAnotherText">Add another</a>' +
+        '</div>';
+    trackerId++;
 }
 
+function closeTracking(trackerId) {
+    document.getElementById("tracker" + trackerId).remove();
+}
+
+function getQueueApi() {
+    // https://ades-fsp.github.io/get-queue
+}
