3d85f6a2967b308e61bbe88e503b6e5af6494229
chunyuancx <chunyuan.19@ichat.sp.edu.sg>
2021-01-12 21:34:28 +0800

    inital state

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+function addTracking(){
+    document.getElementById("grid-container").innerHTML += '<div class="grid-item"><button class="plus-button" onclick=addTracking()></button><a>Add another</a></div>'
+}
+
