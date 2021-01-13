12e20cf4ecb2af443284003c0af5da2ba2a808b1
chunyuancx <64946231+chunyuancx@users.noreply.github.com>
2021-01-13 23:40:30 +0800

    initial state v3
    problems with grid expanding after adding new bottom half box

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
 trackerId = 1;
 
 function addTracking() {
+    //remove original addTracker
     document.getElementById("addTracking").remove();
+    //add tracking box
     document.getElementById("grid-container").innerHTML +=
-        '<div class="grid-item tracking" id="tracker' + trackerId+ '">' +
+        '<div class="grid-item tracking" id="tracker' + trackerId + '">' +
+        '<form action="#" onsubmit="getQueueApi(' + trackerId + ');return false">' +
         '<table class="tableAlign">' +
-        '<form action="#" onsubmit="getQueueApi();return false">' +
         '<tr>' +
         '<td><input type="text" id="companyId" placeholder="Company Id"></td>' +
-        '<td><input type="submit" id="search" value="Search"></td>'+
+        '<td><input type="submit" id="search" value="Search"/></td>' +
         '<td><span class="cross" onclick="closeTracking(' + trackerId + ')">x</span></td>' +
         '<td id="closeTrackingCross"></td>' +
         '</tr>' +
@@ -18,20 +20,40 @@ function addTracking() {
         '<td colspan="2"><input type="checkbox" id="hideInactive" checked>' +
         '<label for="hideInactive">Hide Inactive</label></td>' +
         '</tr>' +
-        '</form>' +
         '</table>' +
+        '</form>' +
         '</div>';
+    //add new addTracker box
     document.getElementById("grid-container").innerHTML +=
         '<div class="grid-item" id="addTracking" onclick=addTracking()>' +
         '<span class="circle plus"></span><a class="addAnotherText">Add another</a>' +
         '</div>';
+    //tracker id count increase for next tracker added
     trackerId++;
 }
 
 function closeTracking(trackerId) {
+    //remove tracker
     document.getElementById("tracker" + trackerId).remove();
 }
 
-function getQueueApi() {
+function getQueueApi(trackerId) {
+    console.log(trackerId);
+
     // https://ades-fsp.github.io/get-queue
+    // do this dylan
+    // i will do the html side after u get all the data i need out ... all queues , queue status
+    // get klanz data out also for graph ask klanz
+    // do validation for the queue according to the example cher showed in video
+
 }
+
+//cy stuff to keep dont delete
+    //draw bottom half box
+    // if (!document.getElementById("bottomhalf" + trackerId)) {
+    //     document.getElementById("tracker" + trackerId).innerHTML +=
+    //         '<div class="bottomHalf" id="bottomhalf' + trackerId + '">' +
+    //         'wahts up' +
+    //         '</div>';
+    // }
+
